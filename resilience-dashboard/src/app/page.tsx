b66cf3f88005e7a709454ab7c27ab6d11d8bcfd4
Srichakra <131296875+chakra121@users.noreply.github.com>
2026-01-01 21:02:42 +0530

    Sample API Test

--- resilience-dashboard/src/app/page.tsx
+++ resilience-dashboard/src/app/page.tsx
@@ -1,9 +1,30 @@
-
+"use client";
+import { Button } from '@/components/ui/button';
+import axios from 'axios';
+import {useState, useEffect, use} from 'react';
 
 export default function Home() {
+  const [message, setMesssage]= useState("Api not fetched");
+  const [count, setCount] = useState(0);
+  const increaseCounter=()=>{
+    setCount(count + 1);
+  }
+  useEffect(() => {
+    axios.get("https://supply-chain-resilience-backend.vercel.app/")
+    .then((response) => {
+      console.log("Data fetched:", response);
+      setMesssage(response.data.message);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+  }, []);
+
   return (
     <div>
-      
+      <h1></h1>
+      <p>{message} & {count}</p>
+      <Button onClick={increaseCounter}>Refresh Page</Button>  
     </div>
   );
 }
